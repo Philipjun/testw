bd0f450772026b50df3ab9601ae4334b59edd00b
Philip Jun <inbox@philipjun.com>
2014-05-01 17:02:39 -0400

    initial model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ws::Application.config.secret_key_base = 'e1f1ca47ef15e50340095596f942c31e71a3f3378f1a347292c32cf90c9a0caad32784a893e0a8384c69af1235aa91b35a0f1b1c3f7920dae44a3c42ce33802b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ws::Application.config.session_store :cookie_store, key: '_ws_session'
